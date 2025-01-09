a038a3cf05dcc182e2e78ed47b7452dfe3764703
Khushi Soni <ksoni.param@gmail.com>
2025-01-09 16:07:02 +0530

    Conversation AI UI

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -4,8 +4,18 @@ import { Menu} from "lucide-react";
 import { Button } from "./ui/button";
 import { SheetTrigger , Sheet, SheetContent } from "@/components/ui/sheet";
 import Sidebar from "./sidebar";
+import { useEffect, useState } from "react";
 
 const MobileSidebar = () => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if(!isMounted) {
+        return null;
+    }
     return ( 
         <div>
             <Sheet>
